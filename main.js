88a04ec2123c64db7b8cc8122c4e66b9471450cb
J-724R <jjpernian@gmail.com>
2023-04-22 18:39:26 -0400

    crawl singel page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+const { crawlPage } = require("./crawl");
+
+crawlPage
+
+function main(){
+  if (process.argv.length < 3){
+    console.log("no website provided");
+    process.exit(1)
+  }
+  if (process.argv.length > 3){
+    console.log("too many website provided");
+    process.exit(1)
+  }
+
+  const baseURL = process.argv[2]
+
+  console.log(`Starting crawling ${baseURL}`);
+  crawlPage(baseURL)
+}
+
+
+// Test url https://wagslane.dev arroung 20 links
+main()
